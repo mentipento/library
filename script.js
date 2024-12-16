d517ea0457215c9a994f22bd5a63889a1a4d80f4
mentipento <26530260+mentipento@users.noreply.github.com>
2024-12-16 20:36:18 +0100

    create files and Book constructor function

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "read" : "not read yet"}`
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+
+console.table(theHobbit);
+console.dir(theHobbit);
