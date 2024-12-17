e2a21bbcd1d6bd15b057e92cf1a9abb9af637a10
mentipento <26530260+mentipento@users.noreply.github.com>
2024-12-17 14:25:39 +0100

    add create new button whithout functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -9,6 +11,43 @@ function Book(title, author, pages, read) {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const mobyDick = new Book("Moby Dick", "Herman Melville", 302, true);
+const prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 279, false);  
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1225, false);  
+const greatExpectations = new Book("Great Expectations", "Charles Dickens", 505, false);  
+const ulysses = new Book("Ulysses", "James Joyce", 730, false);  
+const crimeAndPunishment = new Book("Crime and Punishment", "Fyodor Dostoevsky", 671, true);  
 
-console.table(theHobbit);
-console.dir(theHobbit);
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(mobyDick);
+addBookToLibrary(prideAndPrejudice);
+
+const container = document.querySelector("#container")
+
+myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    container.prepend(card);
+    const title = document.createElement("p");
+    title.classList.add("title");
+    card.appendChild(title);
+    title.textContent = book.title;
+    const author = document.createElement("p");
+    author.classList.add("author");
+    card.appendChild(author);
+    author.textContent = `by ${book.author}`;
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    card.appendChild(pages);
+    pages.textContent = `${book.pages} pages`;
+    const read = document.createElement("p");
+    read.classList.add("read");
+    card.appendChild(read);
+    read.textContent = `${book.read === true ? "read" : "not read"}`;
+});
+
