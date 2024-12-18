3b64c8af7541ad7218add36ba59ea097dfad8b2e
mentipento <26530260+mentipento@users.noreply.github.com>
2024-12-18 20:45:05 +0100

    add last style changes

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "read" : "not read yet"}`
-    }
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "read" : "not read yet"}`;
+    };
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 const mobyDick = new Book("Moby Dick", "Herman Melville", 302, true);
-const prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 279, false);  
-const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1225, false);  
-const greatExpectations = new Book("Great Expectations", "Charles Dickens", 505, false);  
-const ulysses = new Book("Ulysses", "James Joyce", 730, false);  
-const crimeAndPunishment = new Book("Crime and Punishment", "Fyodor Dostoevsky", 671, true);  
-
+const prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 279, false);
+const warAndPeace = new Book("War and Peace", "Leo Tolstoy", 1225, false);
+const greatExpectations = new Book("Great Expectations", "Charles Dickens", 505, false);
+const ulysses = new Book("Ulysses", "James Joyce", 730, false);
+const crimeAndPunishment = new Book("Crime and Punishment", "Fyodor Dostoevsky", 671, true);
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
@@ -27,10 +26,9 @@ addBookToLibrary(theHobbit);
 addBookToLibrary(mobyDick);
 addBookToLibrary(prideAndPrejudice);
 
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
-
-    function createBookCard(book) {
+function createBookCard(book) {
     const card = document.createElement("div");
     card.classList.add("card");
     container.append(card);
@@ -52,29 +50,24 @@ const container = document.querySelector("#container")
 
     const readContainer = document.createElement("div");
     readContainer.classList.add("read-container");
-    
-    // Checkbox erstellen
+
     const readInput = document.createElement("input");
     readInput.type = "checkbox";
     readInput.id = `read-${book.title}`;
     readInput.checked = book.read;
-    
-    // Label erstellen
+
     const readLabel = document.createElement("label");
     readLabel.textContent = "Read";
     readLabel.htmlFor = `read-${book.title}`;
-    
-    // Checkbox und Label zur Karte hinzufügen
+
     readContainer.appendChild(readInput);
     readContainer.appendChild(readLabel);
     card.appendChild(readContainer);
-    
-    
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete-button");
-    card.appendChild(deleteButton);
     deleteButton.textContent = "";
+    card.appendChild(deleteButton);
 
     const newBookDialog = document.querySelector("#delete-book-dialog");
 
@@ -83,54 +76,43 @@ const container = document.querySelector("#container")
         newBookDialog.showModal();
     });
 
-      document.querySelector(".delete-book-no").addEventListener("click", () => {
+    document.querySelector(".delete-book-no").addEventListener("click", () => {
         newBookDialog.close();
     });
 
     document.querySelector("#delete-book-yes").addEventListener("click", () => {
         if (bookToDelete) {
             container.removeChild(bookToDelete);
-            bookToDelete = null; // Rücksetzen nach dem Löschen
+            bookToDelete = null;
         }
         newBookDialog.close();
     });
-};
+}
 
-myLibrary.forEach((book) => createBookCard(book))
+myLibrary.forEach((book) => createBookCard(book));
 
 const dialog = document.querySelector("#new-book-dialog");
 
-
 document.querySelector(".add-btn").addEventListener("click", () => {
     dialog.showModal();
-})
+});
 
 document.querySelector("#cls-btn").addEventListener("click", () => {
     dialog.close();
-})
+});
 
 document.querySelector("#form").addEventListener("submit", (event) => {
     event.preventDefault();
 
     const nameInput = document.querySelector("#name").value;
-    console.log("Book title:", nameInput);
-
     const authorInput = document.querySelector("#author").value;
-    console.log("Author:", authorInput);
-
     const pagesInput = document.querySelector("#pages").value;
-    console.log("Number of pages:", pagesInput);
-
-    const readInput = document.querySelector("#read").value;
-    console.log("Read:", readInput);
+    const readInput = document.querySelector("#read").checked;
 
     const newBook = new Book(nameInput, authorInput, pagesInput, readInput);
     addBookToLibrary(newBook);
-
     createBookCard(newBook);
 
     document.querySelector("#form").reset();
-
     dialog.close();
-}
-)
+});
